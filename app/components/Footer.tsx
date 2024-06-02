dd2437baecc1dc87f39e6a97c78d927eccaa109f
ibrahimmoamen100 <ibrahim.moamen100@gmail.com>
2024-06-03 02:20:42 +0300

    update

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,9 +1,7 @@
 import React from "react";
-import Image from "next/image";
-import logo from "@/public/images/tarteel.png";
 
 function Footer() {
-  return <div></div>;
+  return <div>Footer</div>;
 }
 
 export default Footer;
