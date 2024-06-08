ff84701acb89871e892d86da3f0a181cb5d131b6
ibrahimmoamen100 <ibrahim.moamen100@gmail.com>
2024-06-08 17:03:42 +0300

    new update

--- middleware.ts
+++ middleware.ts
@@ -1,21 +1,20 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-
+import withAuth from "next-auth/middleware";
 // This function can be marked `async` if using `await` inside
 
-export function middleware(request: NextRequest) {}
-
-// See "Matching Paths" below to learn more
-export const config = {
-  matcher: [
-    "/profile/:path*",
-    "/dashboard/:path*",
-    {
-      source: "/((?!api|_next/static|_next/image|favicon.ico).*)",
-      missing: [
-        { type: "header", key: "next-router-prefetch" },
-        { type: "header", key: "purpose", value: "prefetch" },
-      ],
-    },
-  ],
-};
+// export default withAuth(function middleware(request: NextRequest) {});
+// // See "Matching Paths" below to learn more
+// export const config = {
+//   matcher: [
+//     "/profile/:path*",
+//     "/dashboard/:path*",
+//     {
+//       source: "/((?!api|_next/static|_next/image|favicon.ico).*)",
+//       missing: [
+//         { type: "header", key: "next-router-prefetch" },
+//         { type: "header", key: "purpose", value: "prefetch" },
+//       ],
+//     },
+//   ],
+// };
